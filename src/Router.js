891bc3fd4376f0b71c199702016c6daaedc6017b
sumin <tnals545@naver.com>
2022-12-07 16:23:33 +0900

    Chore - Routing setting

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "App";
+import GlobalStyles from "./styles/GlobalStyles";
+import SignUp from "./pages/SginUp";
+// import Todo from "pages/Todo";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="sign_up" element={<SignUp />} />
+        {/* <Route path="todo" element={<Todo />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
