ea5c4a911bb681059a8f2511fc0b9496403804ef
sumin <tnals545@naver.com>
2022-12-20 19:46:11 +0900

    Update - import 절대경로로 변경

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "App";
-import GlobalStyles from "./styles/GlobalStyles";
-import SignUp from "./pages/SginUp";
+import GlobalStyles from "styles/GlobalStyles";
+import SignUp from "pages/SginUp";
 import Todo from "pages/Todo";
 
 const Router = () => {
